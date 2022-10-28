ee6b5497d2321ada10dc7529d69b7db1ca3e009c
Bacuuu <823727097@qq.com>
2022-10-28 15:05:34 +0800

    feat:更改为离线canvas，避免页面体现加载过程

--- canvasTool.js
+++ canvasTool.js
@@ -2,35 +2,70 @@ import { tileImgSize } from './constant.js'
 import globalData from './globalData.js'
 let ctx = null
 let canvas = null
+let offlineCanvas = null // 离线canvas
+let offlineCtx = null
 const setCtx = function (_canvas) {
   canvas = _canvas
   ctx = canvas.getContext('2d')
+  offlineCanvas = document.createElement('canvas')
+  offlineCanvas.width = canvas.width
+  offlineCanvas.height = canvas.height
+  offlineCtx = offlineCanvas.getContext('2d')
 }
 
 // 加载单个图片
 const addSingleImage = function (url, x, y) {
-  const img = new Image(256, 256)
-  // 先去indexeddb查询数据
-  globalData.db.readData('tiles', url).then(r => {
-    img.src = window.URL.createObjectURL(r.data)
-  }).catch(() => {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
-    xhr.responseType = 'blob'
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        globalData.db.writeTo('tiles', {
-          id: url,
-          data: xhr.response
-        })
-        img.src = window.URL.createObjectURL(xhr.response)
+  return new Promise((rs, rj) => {
+    const img = new Image(256, 256)
+    // 先去indexeddb查询数据
+    globalData.db.readData('tiles', url).then(r => {
+      img.src = window.URL.createObjectURL(r.data)
+    }).catch(() => {
+      const xhr = new XMLHttpRequest()
+      xhr.open('GET', url)
+      xhr.responseType = 'blob'
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          globalData.db.writeTo('tiles', {
+            id: url,
+            data: xhr.response
+          })
+          img.src = window.URL.createObjectURL(xhr.response)
+        }
       }
+      xhr.send()
+    })
+    img.onload = () => {
+      offlineCtx.drawImage(img, 0, 0, 256, 256, x, y, ...tileImgSize)
+      rs()
     }
-    xhr.send()
+    img.onerror = () => {
+      rj()
+    }
   })
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0, 256, 256, x, y, ...tileImgSize)
+}
+
+const reloadTile = function (center, replaceUrlFunc) {
+  let columns = Math.floor(globalData.canvasWh[0] / tileImgSize[0]) + 1
+  columns += columns % 2
+  let rows = Math.floor(globalData.canvasWh[1] / tileImgSize[1]) + 1
+  rows += rows % 2
+  globalData.grid = [columns + 1, rows + 1]
+  const offset = [globalData.canvasWh[0] / 2 - globalData.tileOffset[0], globalData.canvasWh[1] / 2 - globalData.tileOffset[1]]
+  globalData.centerTile = center
+  const promiseList = []
+  // clearOfflineCanvas()
+  for (let c = -columns / 2; c <= columns / 2; c ++) {
+    for (let r = -rows / 2; r <= rows / 2; r ++) {
+      let _center = center
+      promiseList.push(addSingleImage(replaceUrlFunc(_center[0] + c, _center[1] + r, _center[2]), c * tileImgSize[0] + offset[0],r * tileImgSize[1] + offset[1]))
+    }
   }
+  Promise.all(promiseList)
+    .then(r => {
+      clearCanvas()
+      ctx.drawImage(offlineCanvas, 0, 0)
+    })
 }
 
 const addText = function (content, x, y) {
@@ -42,6 +77,10 @@ const resizeTo = function (width, height) {
     canvas.width = width
     canvas.height = height
   }
+  if (offlineCanvas) {
+    offlineCanvas.width = width
+    offlineCanvas.height = height
+  }
 }
 
 const cavasZoom = function (cb) {
@@ -52,6 +91,7 @@ const cavasZoom = function (cb) {
 
 // 画布平移
 const canvasMove = function (x, y) {
+  // canvasTranslate(x, y)
   // 如果是右下角移动，那么x,y都是正的
   // 对于偏移来说，图片右下移动，偏移量减少，更偏向于图片原点
   const currentOffsetX = -x + globalData.tileOffset[0]
@@ -73,11 +113,25 @@ const canvasMove = function (x, y) {
   // })
 }
 
+const canvasTranslate = function (x, y) {
+  ctx.translate(x, y)
+}
+
+const clearCanvas = function () {
+  ctx.clearRect(0, 0, ...globalData.canvasWh)
+}
+
+const clearOfflineCanvas = function () {
+  offlineCtx.clearRect(0, 0, ...globalData.canvasWh)
+}
+
 export default {
   setCtx,
   addSingleImage,
   resizeTo,
   cavasZoom,
+  reloadTile,
   addText,
-  canvasMove
+  canvasMove,
+  canvasTranslate
 }
